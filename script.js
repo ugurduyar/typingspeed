fa73b27299ec54beb6444dd0b10bbcf2d4f72641
ugurduyar <ismailugurduyar@gmail.com>
2020-06-08 10:02:05 +0300

    words array added

--- script.js
+++ script.js
@@ -7,3 +7,27 @@ const settingsBtn = document.getElementById("settings-btn");
 const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
 const difficultySelect = document.getElementById("difficulty");
+
+// List of words
+const words = [
+  "sigh",
+  "tense",
+  "airplane",
+  "ball",
+  "pies",
+  "juice",
+  "warlike",
+  "bad",
+  "north",
+  "dependent",
+  "steer",
+  "silver",
+  "highfalutin",
+  "superficial",
+  "quince",
+  "eight",
+  "feeble",
+  "admit",
+  "drag",
+  "loving",
+];
